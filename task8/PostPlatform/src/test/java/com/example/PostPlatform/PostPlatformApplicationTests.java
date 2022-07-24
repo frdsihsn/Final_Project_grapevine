15238d2fb8111e1a3da8a1fe26aa7a62a1f7a659
Jeryl Teo <jerylteojoonkit@hotmail.com>
2022-07-24 21:40:21 +0800

    added task8 java files

---
+++ task8/PostPlatform/src/test/java/com/example/PostPlatform/PostPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PostPlatform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
