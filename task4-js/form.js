45199b9950fd390fc7889dcc5cc7468eb4961a8c
Firdaus <firdausihsan97@gmail.com>
2022-07-28 14:17:49 +0800

    use fetch to connect frontend & backend

--- task4-js/form.js
+++
@@ -1,8 +0,0 @@
-function toggleForm() {
-  var i = document.getElementById("addForm");
-  if (i.style.display === "block") {
-    i.style.display = "none";
-  } else {
-    i.style.display = "block";
-  }
-}
--- task8/PostPlatform/src/main/java/com/example/PostPlatform/repos/PostRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.PostPlatform.repos;
-
-import com.example.PostPlatform.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-}
